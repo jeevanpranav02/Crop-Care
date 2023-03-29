d501d0b61d3001a1b559b2fed3b3b7276ed96e2f
Pranav Jothimani <pranav.2001146@srec.ac.in>
2023-03-30 01:24:20 +0530

    Updated constant file

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
-const Color primaryColor = Color(0xFFFFFFFF);
+const Color primaryColor = Color(0xFF87E4D5);
 const Color secondaryColor = Color(0xFF00BFA5);
+const Color onPrimaryColor = Color(0xFFFFFFFF);
 const Color textColor = Color(0xFFFFFFFF);
 const Color textColor2 = Color(0xFF000000);
+const Color textColor2Lite = Color(0xFF696868);
+const Color iconColor = Color(0xFF242222);
+MaterialColor primaryColorSwatch = MaterialColor(
+  primaryColor.value,
+  const <int, Color>{
+    50: Color(0xFFE0F2F1),
+    100: Color(0xFFB2DFDB),
+    200: Color(0xFF80CBC4),
+    300: Color(0xFF4DB6AC),
+    400: Color(0xFF26A69A),
+    500: Color(0xFF009688),
+    600: Color(0xFF00897B),
+    700: Color(0xFF00796B),
+    800: Color(0xFF00695C),
+    900: Color(0xFF004D40),
+  },
+);
